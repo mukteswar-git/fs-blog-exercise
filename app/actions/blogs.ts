7998e42869336bc0ea5a98f8a0ef1fdc7e8b5922
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-09-26 11:08:14 +0530

    Complete exercise 13

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -5,15 +5,29 @@ import { revalidatePath } from "next/cache"
 import { addBlog, increaseLike } from "../services/blogs"
 import { auth } from "@/auth"
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (
+  prevState: { error: string },
+  formData: FormData,
+) => {
   const session = await auth()
   if (!session) {
     redirect("/login")
   }
 
   const title = formData.get("title") as string
+  if (!title || title.length < 5) {
+    return { error: "Title must be at least 5 characters long"}
+  }
+
   const author = formData.get("author") as string
+  if (!author || author.length < 5) {
+    return { error: "Author must be at least 5 characters long"}
+  }
+  
   const url = formData.get("url") as string
+  if (!url || url.length < 5) {
+    return { error: "URL must be at least 5 characters long"}
+  }
   
   await addBlog(title, author, url)
 
--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,29 +1,35 @@
-import { createBlog } from "@/actions/blogs";
+"use client"
+
+import { useActionState } from "react";
+import { createBlog } from "@/actions/blogs"
 
 const NewBlog = () => {
+  const [state, formAction] = useActionState(createBlog, { error: "" })
+
   return (
     <div className="blog-page">
       <h2 className="mt-4">Create a new blog</h2>
 
-      <form className="blog-form" action={createBlog}>
+      <form className="blog-form" action={formAction}>
         <div className="form-field">
           <label htmlFor="title">Title</label>
-          <input id="title" type="text" name="title" required />
+          <input id="title" type="text" name="title" required minLength={5} />
         </div>
 
         <div className="form-field">
           <label htmlFor="author">Author</label>
-          <input id="author" type="text" name="author" required />
+          <input id="author" type="text" name="author" required minLength={5} />
         </div>
 
         <div className="form-field">
           <label htmlFor="url">URL</label>
-          <input id="url" type="url" name="url" required />
+          <input id="url" type="url" name="url" required minLength={5} />
         </div>
 
         <button type="submit" className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
           Create
         </button>
+        {state.error && <p style={{ color: "red" }}>{state.error}</p>}
       </form>
     </div>
   );
